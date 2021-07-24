40e25221b5e1e8a124101c8f6c3b6cdc7c32b3ec
BakhvaJakeli <lordbaxva@gmail.com>
2021-07-24 15:08:39 +0400

    Initial

---
+++ Final_chatProject/Final_chatProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Final_chatProject
+//
+//  Created by Baxva Jakeli on 24.07.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
